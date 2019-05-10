ea5c76aff8c96b69c51a04a51e2397def625a12f
Welni <kris2604@o2.pl>
2019-05-10 16:13:43 +0200

    bug fixed in simplified spacestate, added sine wawe generator

--- plots.m
+++ plots.m
@@ -7,11 +7,12 @@ phi = simout.data(:,4);
 th = simout.data(:,5);
 psi = simout.data(:,6);
 plot3(x, y, z);
-figure('Name', 'simple');
+hold on
 x = simout1.data(:,1);
 y = simout1.data(:,2);
 z = simout1.data(:,3);
 phi = simout1.data(:,4);
 th = simout1.data(:,5);
 psi = simout1.data(:,6);
-plot3(x, y, z);
+plot3(x, y, z, '-r');
+hold off
